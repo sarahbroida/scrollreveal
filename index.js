7189bfaf0fbbc33191c8b44e681495674510ea9b
sarahbroida <156728661+sarahbroida@users.noreply.github.com>
2024-02-21 18:04:31 +0000

    Added code from videos

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add JavaScript below
+
+ScrollReveal().reveal('.sarah' , {delay: 1000});
+
+/*
+function clickOMatic() {
+    alert('Clicked!');
+}
+
+window.onload = function() {
+    const click = document.getElementById('click');
+
+click.addEventListener("click", (event) => {
+    clickOMatic();
+});
+};
+
+*/
