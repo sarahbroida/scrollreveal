b44af0c336e41ad3e9b4d07106c10e5b2b06d4e5
sarahbroida <156728661+sarahbroida@users.noreply.github.com>
2024-02-22 20:07:41 +0000

    First attempt at Scroll Reveal

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Add JavaScript below
 
-ScrollReveal().reveal('.sarah' , {delay: 1000});
+
+window.onload = function() {
+ScrollReveal().reveal('.BigBen' , {delay: 500} , {duration: 5000});
+ScrollReveal().reveal('.VanGoghSunflowers' , {delay: 1000} , {duration: 1000});
+ScrollReveal().reveal('.AfternoonTea' , {delay: 1100} , {duration: 11000});
+ScrollReveal().reveal('.EiffelTower_Selfie' , {delay: 900} , {duration: 90});
+ScrollReveal().reveal('.Croissant' , {delay: 700} , {duration: 7000});
+}
+
+
+
+
+
+
 
 /*
 function clickOMatic() {
